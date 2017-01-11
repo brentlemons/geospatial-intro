6d2fc08adcfde2a9f93afa185030ce3d123a02d1
Brent Lemons <brent_lemons@yahoo.com>
2017-01-11 08:09:21 -0600

    geospatial talk

---
+++ src/main/java/com/fltck/demo/geospatial/GeospatialIntroApplication.java
@@ -0,0 +1,12 @@
+package com.fltck.demo.geospatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeospatialIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeospatialIntroApplication.class, args);
+	}
+}
